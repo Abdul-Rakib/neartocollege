07da2e29f0e0f313c38ba5eb14713b016db0391a
abdulrakib <firefalls2004@gmail.com>
2024-07-08 22:14:30 +0530

    host

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     },
   },
   server: {
-    // host: "0.0.0.0",
+    host: "0.0.0.0",
     proxy: {
       "/api": {
         target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com",
